87f5f1df07ca5909e2fd196a2691a7c69db090ef
MarcValentines <marcvalentinesmiro@gmail.com>
2024-12-02 17:07:04 +0100

    Se genera un random para lanzar una moneda y se empieza con el sistema de rondas
    En este caso se empieza con el sistema de rondas y se determina el sistema para lanzar una moneda con el random, de forma que queda de forma aleatória quien empieza

--- src/tres_en_ralla_code/Code.java
+++ src/tres_en_ralla_code/Code.java
@@ -1,15 +1,21 @@
 package tres_en_ralla_code;
 import java.util.Scanner;
+import java.util.Random;
 public class Code {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner entradaTec = new Scanner(System.in);
+		Random random = new Random();
 		
 		String[][] Tablero = {{"-", "-", "-"},
 				  {"-", "-", "-"},
 				  {"-", "-", "-"}};
 		int posicTablero = 9;
+		int numRondJ1 = 0;
+		int numRondJ2 = 0;
+		int contFil = 0;
+		int contColumn = 0;
 		
 		System.out.println("|---Bienvenido al 3 en ralla!!!---|");
 		System.out.println("Las normas son simples, se van a mostrar a continuación");
@@ -21,11 +27,26 @@ public class Code {
 		System.out.println();
 		
 		//ingresar los nombres de los jugadores
-		System.out.println("inserta el nombre del jugador 1 = ");
+		System.out.print("inserta el nombre del jugador 1 = ");
 		String jugador1 = entradaTec.nextLine();
 		
-		System.out.println("Inserta el nombre del jugador 2 = ");
+		System.out.println();
+		
+		System.out.print("Inserta el nombre del jugador 2 = ");
 		String jugador2 = entradaTec.nextLine();
+		
+		System.out.println("Se va a lanzar una moneda, si sale cara empieza " + jugador1);
+		System.out.println("Si sale cruz empieza " + jugador2);
+		
+		int moneda = random.nextInt(2) + 1;
+		
+		if (moneda == 1) {
+			System.out.println("Ha salido cara!!");
+			numRondJ1++;
+		}else if (moneda == 2) {
+			System.out.println("Ha salido cruz!!");
+			numRondJ2++;
+		}
 		
 		//mostrar el tablero inicialmente
 		for (int i = 0; i < Tablero.length; i++) {
@@ -39,6 +60,7 @@ public class Code {
 		for (int i = 0; i < posicTablero; i++) {
 			int rondas = i + 1;
 			System.out.println("Ronda " + rondas);
+			
 			
 		}
 		
