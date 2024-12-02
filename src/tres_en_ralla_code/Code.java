eda85e48f3308a9d944a02246d08cbeb4d336ee6
MarcValentines <marcvalentinesmiro@gmail.com>
2024-12-02 16:46:47 +0100

    Nombres jugadores y se muestra el tablero
    Se muestra el tablero y se pregunta a los jugadores por el nombre

--- src/tres_en_ralla_code/Code.java
+++ src/tres_en_ralla_code/Code.java
@@ -12,13 +12,29 @@ public class Code {
 		
 		System.out.println("|---Bienvenido al 3 en ralla!!!---|");
 		System.out.println("Las normas son simples, se van a mostrar a continuación");
+		System.out.println();
 		System.out.println("Se lanzará una \"moneda\" (se escoge aleatoriamente) para elegir quien empezara primero, el jugador 1 o el jugador 2.");
 		System.out.println("Los dos jugadores irán poniendo las fichas hasta que: ");
 		System.out.println(" - Uno de los dos jugadores haya conseguido poner 3 fichas en línea, ya sea horizontalmente, verticalmente o diagonalmente.");
 		System.out.println(" - Haya un empate debido a que no quedan mas huecos libres en el tablero.");
+		System.out.println();
 		
+		//ingresar los nombres de los jugadores
+		System.out.println("inserta el nombre del jugador 1 = ");
+		String jugador1 = entradaTec.nextLine();
 		
-						
+		System.out.println("Inserta el nombre del jugador 2 = ");
+		String jugador2 = entradaTec.nextLine();
+		
+		//mostrar el tablero inicialmente
+		for (int i = 0; i < Tablero.length; i++) {
+			for (int j = 0; j < Tablero[i].length; j++) {
+				System.out.print(Tablero[i][j] + " ");
+			}
+			System.out.println();
+		}
+	
+		
 	}
 
 }
