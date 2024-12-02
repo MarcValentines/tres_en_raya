030467e3c9048d92e6054a28a6227e8a00e0bb50
MarcValentines <marcvalentinesmiro@gmail.com>
2024-12-02 16:41:57 +0100

    Explicación reglas y declaración matriz tablero
    Se explican las normas del juego y se establece un tablero en el que reemplazar el contenido por una "x" o una "o"

--- src/tres_en_ralla_code/Code.java
+++ src/tres_en_ralla_code/Code.java
@@ -6,6 +6,19 @@ public class Code {
 		// TODO Auto-generated method stub
 		Scanner entradaTec = new Scanner(System.in);
 		
+		String[][] Tablero = {{"-", "-", "-"},
+				  {"-", "-", "-"},
+				  {"-", "-", "-"}};
+		
+		System.out.println("|---Vienvenido al 3 en ralla!!!---|");
+		System.out.println("Las normas son simples, se van a mostrar a continuación");
+		System.out.println("Se lanzará una \"moneda\" (se escoge aleatoriamente) para elegir quien empezara primero, el jugador 1 o el jugador 2.");
+		System.out.println("Los dos jugadores irán poniendo las fichas hasta que: ");
+		System.out.println(" - Uno de los dos jugadores haya conseguido poner 3 fichas en línea, ya sea horizontalmente, verticalmente o diagonalmente.");
+		System.out.println(" - Haya un empate debido a que no quedan mas huecos libres en el tablero.");
+		
+		
+						
 	}
 
 }
