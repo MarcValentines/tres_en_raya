b331fc2ff83eea176d341b9316dbb8a46257a0ee
MarcValentines <marcvalentinesmiro@gmail.com>
2024-12-02 16:31:39 +0100

    Creación de los archivos principales y el proyecto
    Se crea el proyecto de 3 en ralla y el script principal

---
+++ src/tres_en_ralla_code/Code.java
@@ -0,0 +1,11 @@
+package tres_en_ralla_code;
+import java.util.Scanner;
+public class Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entradaTec = new Scanner(System.in);
+		
+	}
+
+}
